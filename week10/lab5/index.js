cc3116e9215b5ee68e18c9ec29e5b881e7c20422
Gillian <gillianldowney12@gmail.com>
2025-03-13 20:29:01 -0700

    lab10 done

--- week10/lab5/index.js
+++ week10/lab5/index.js
@@ -37,6 +37,19 @@ const shuffle = (list) => {
 */
 
 /*Your code goes here for step 1*/
+const urls = [
+    "https://i.ibb.co/sjvVrG3/fruit-plyh.jpg",
+    "https://i.ibb.co/g3tqj4w/fruit-zxcv.jpg",
+    "https://i.ibb.co/88Hy4rD/fruit-wert.jpg",
+    "https://i.ibb.co/NmNMLKF/fruit-astt.jpg",
+    "https://i.ibb.co/qp8Q1G6/fruit-dfgh.jpg",
+    "https://i.ibb.co/9thfVhB/fruit-hjkl.jpg",
+    "https://i.ibb.co/YtTfqvD/fruit-dcnj.jpg",
+    "https://i.ibb.co/CVwKM8Z/fruit-ecuh.jpg",
+    "https://i.ibb.co/ccNyKX0/fruit-uzxc.jpg"
+];
+
+let randomUrls = shuffle(urls);
 
 
 
@@ -81,8 +94,13 @@ const images = document.querySelectorAll('.card img');
 */
 
 /*Your code goes here for step 2*/
+let displayImage = (images, randomUrls) =>{
+    images.forEach((image, index)=>{
+        image.src = randomUrls[index];
+    });
+};
 
-
+displayImage(images, randomUrls);
 
 
 const h1 = document.querySelector('h1');
